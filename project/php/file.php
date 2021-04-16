95915d01a30b2687fbb8ad58b01a63cebb53dbcc
07anshul <75933710+07anshul@users.noreply.github.com>
2021-04-17 00:27:52 +0530

    Update file.php

--- project/php/file.php
+++ project/php/file.php
@@ -1,7 +1,7 @@
 <?php
 
 echo "what is the problem";
-$myfile = fopen("../messages/21-04-08/11/02:04:03, "r);
+$myfile = fopen("../messages/21-04-08/11/02:04:03", "r");
 while (!feof($myfile)) {
    echo fgets($myfile).'<br>';
 }
