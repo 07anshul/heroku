a0f5bbe185bfeb487ffed2449c0a4b9569b41ac7
07anshul <75933710+07anshul@users.noreply.github.com>
2021-04-17 00:35:45 +0530

    Update file.php

--- project/php/file.php
+++ project/php/file.php
@@ -1,9 +1,6 @@
 <?php
 
 echo "what is the problem";
-$myfile = fopen("../messages/ww", "w");
-$text = "wowowowowowwo";
-fwrite($myfile, $text);
 $myfile = fopen("../messages/ww", "r");
 while (!feof($myfile)) {
    echo fgets($myfile).'<br>';
