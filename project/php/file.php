5e14329cd740b240308fd83ae049e5961b97a4f9
07anshul <75933710+07anshul@users.noreply.github.com>
2021-04-17 00:30:32 +0530

    Update file.php

--- project/php/file.php
+++ project/php/file.php
@@ -1,8 +1,7 @@
 <?php
 
 echo "what is the problem";
-$myfile = fopen("../messages/21-04-08/11/02:04:03", "r");
-while (!feof($myfile)) {
-   echo fgets($myfile).'<br>';
-}
+$myfile = fopen("../messages/ww", "w");
+$text = "wowowowowowwo";
+fwrite($my_file, $text);
 ?>
